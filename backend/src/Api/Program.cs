7ab6e0a44fe0a3cd65671601d802cb88146f448f
Magnus Halvorsen <nomaghal@gmail.com>
2022-09-07 16:42:58 +0200

    Set default https_port

--- backend/src/Api/Program.cs
+++ backend/src/Api/Program.cs
@@ -46,10 +46,8 @@ if (app.Environment.IsDevelopment())
 else
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    //app.UseHsts();
-    app.UseHttpsRedirection();
+    app.UseHsts();
 }
-
 
 app.UseHttpsRedirection();
 app.UseCors(myAllowSpecificOrigins);
