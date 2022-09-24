410f32d712f6cf70373028a859bc584ca40ce2e7
Magnus Halvorsen <nomaghal@gmail.com>
2022-09-24 12:04:44 +0200

    Add health check

---
+++ backend/src/Api/Controllers/HealthCheckController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Munin.Application.TodoItems.Queries.GetTodoItemsWithPagination;
+
+namespace Munin.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HealthCheckController : ControllerBase
+{
+    [HttpGet]
+    public string Get()
+    {
+        return "Ok";
+    }
+}
